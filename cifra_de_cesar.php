56baa026b3a0615fa3fb18eca776e791153ccb74
HugoLSPaulo <64653369+HugoLSPaulo@users.noreply.github.com>
2023-01-02 09:48:07 +0100

    up

--- cifra_de_cesar.php
+++ cifra_de_cesar.php
@@ -1,21 +1,14 @@
 <?php
 
-$texto_claro = "domingos";
-
+$texto_claro = "hugo";
 $letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
 $aux = array();
-
 for ($y = 0; $y <= strlen($texto_claro); $y++) {
-
     for ($i = 0; $i <= 26; $i++) {
-
         if ($letras[$i] == $texto_claro[$y]) {
-
             $aux[] = $letras[$i + 3];
         }
     }
 }
-
 $re = implode("", $aux);
 echo $re;
