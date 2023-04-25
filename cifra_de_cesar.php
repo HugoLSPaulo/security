960d4d4d13dc7c6ef54f64a8c7cf08b4d17591e6
HugoLSPaulo <64653369+HugoLSPaulo@users.noreply.github.com>
2023-04-25 12:09:43 +0100

    cifra de cesar

--- cifra_de_cesar.php
+++ cifra_de_cesar.php
@@ -1,5 +1,6 @@
 <?php
 
+//Criptografia
 $texto_claro = "hugo";
 $letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 $aux = array();
@@ -9,6 +10,22 @@ for ($y = 0; $y <= strlen($texto_claro); $y++) {
             $aux[] = $letras[$i + 3];
         }
     }
+}
+$re = implode("", $aux);
+echo $re;
+
+//Decriptografia
+$letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+$txt_crip = "kxjr";
+for($y = 0;$y<strlen($txt_crip);$y++){
+    for($i = 0; $i<26; $i++){
+          if ($letras[$i] == $txt_crip[$y]) {
+            $aux[] = $letras[$i - 3];
+        }
+
+    }
+
 }
 $re = implode("", $aux);
 echo $re;
