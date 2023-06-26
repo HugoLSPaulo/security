b9591c94d7c7476cebf127f1e4f5c010352d7e10
HugoLSPaulo <64653369+HugoLSPaulo@users.noreply.github.com>
2023-06-26 12:50:32 +0100

    cesar

--- cifra_de_cesar.php
+++ cifra_de_cesar.php
@@ -1,7 +1,7 @@
 <?php
 
 //Criptografia
-$texto_claro = "hugo";
+$texto_claro = "navara te amo";
 $letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 $aux = array();
 for ($y = 0; $y <= strlen($texto_claro); $y++) {
@@ -17,7 +17,7 @@ echo $re;
 //Decriptografia
 $letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-$txt_crip = "kxjr";
+$txt_crip = "qdydudwhdpr";
 for($y = 0;$y<strlen($txt_crip);$y++){
     for($i = 0; $i<26; $i++){
           if ($letras[$i] == $txt_crip[$y]) {
